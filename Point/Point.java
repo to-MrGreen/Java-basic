ba96a9b33e8461f1c706851737496879d9cd960e
Md. Samiul Haque <67146767+to-MrGreen@users.noreply.github.com>
2020-06-25 16:54:24 +0600

    Add files via upload

---
+++ Point/Point.java
@@ -0,0 +1,32 @@
+
+/**
+ * Write a description of Point here.
+ * 
+ * @author (Md.Samiul Hauqe) 
+ * @version (a version number or a date)
+ */
+import edu.duke.*;
+public class Point {
+    private int x;
+    private int y;
+    public Point(int startx, int starty){
+        x = startx;
+        y = starty;
+    }
+    public int getX(){
+        return x;
+    }
+    public int getY(){
+        return y;
+    }
+    public double distance(Point otherPt){
+        int dx = x - otherPt.getX();
+        int dy = y - otherPt.getY();
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+    public static void main(String[]args){
+        Point p1 =new Point(3,4);
+        Point p2 = new Point(6,4);
+        System.out.println(p1.distance(p2));
+    }
+}
